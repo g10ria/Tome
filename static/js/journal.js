d00d62920d16b0c945616ff4088738153677b82d
g10ria <22gloriaz@students.harker.org>
2020-03-01 01:01:17 -0800

    added submitting journal entries to journal page

--- static/js/journal.js
+++ static/js/journal.js
@@ -13,6 +13,25 @@ var app = new Vue({
             error: false,
             text: ""
         },
+        entryName: "",
+        entryContent: "",
+        colorOptions: [{
+            name: "Apricot",
+            hex: "F29E4C"
+        }, {
+            name: "Storm",
+            hex: "048BA8"
+        }, {
+            name: "Heather",
+            hex: "787878"
+        }, {
+            name: "Plum",
+            hex: "A4036F"
+        }, {
+            name: "Wheat",
+            hex: "F3C969"
+        }],
+        selectedColor: 0,
         drawer: false,
         entries: [] // populate on created
     },
@@ -47,11 +66,35 @@ var app = new Vue({
                 location.reload()
             })
         },
+        selectColor: function(index) {
+            this.selectedColor =  index
+        },
         goto: function (url) {
             window.location.href = url
         },
         niceDate: function (date) {
             return date.toString().substring(4, 15)
+        },
+        submitJournal: function () {
+            makeRequest("POST", "/user/journal", {
+                name: this.entryName,
+                content: this.entryContent,
+                date: new Date().toISOString(),
+                color: this.colorOptions[this.selectedColor].hex
+            }, function (res) {
+                // todo check success/error
+                this.entryName = ""
+                this.entryContent = ""
+                this.selectedColor = 0
+
+                let body = JSON.parse(res.responseText)
+                body["date"] = this.niceDate(new Date(body["date"]))
+
+                this.entries.push(body)
+            }.bind(this))
+        },
+        openEntry: function(index) {
+            
         }
     }
 })
